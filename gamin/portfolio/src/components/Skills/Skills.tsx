4ab6294ae1264c0c65cd899f77d423b05d6ac4ab
Ga Min Cha <prettygamin@naver.com>
2026-03-01 14:50:18 +0900

    수정: Skills 컴포넌트-cdn이용으로 변경

--- gamin/portfolio/src/components/Skills/Skills.tsx
+++ gamin/portfolio/src/components/Skills/Skills.tsx
@@ -1,17 +1,49 @@
-import SkillCategory from "../SkillCategory/SkillCategory";
-import { SKILLS_DATA } from "./data";
+import { techMap } from "../../utils/getIcons";
 import styles from "./Skills.module.css";
 
 function Skills() {
+  const mySkillSet = [
+    { category: "Language", techNames: ["JavaScript", "TypeScript"] },
+    { category: "Frontend", techNames: ["React", "React Query", "Redux"] },
+    {
+      category: "Backend",
+      techNames: ["Express", "Node.js"],
+    },
+    {
+      category: "Others",
+      techNames: ["MySQL", "PostgreSQL", "Supabase", "Vercel"],
+    },
+  ];
+
   return (
     <div className={styles.skills}>
-      {SKILLS_DATA.map((data) => (
-        <SkillCategory
-          key={data.category}
-          category={data.category}
-          items={data.items}
-        />
-      ))}
+      {/* 각 카테고리 그룹에 대하여 */}
+      {mySkillSet.map((group) => {
+        const techItems = group.techNames.map((name) =>
+          techMap.get(name.toLowerCase()),
+        );
+        return (
+          <section key={group.category}>
+            <h2>{group.category}</h2>
+
+            <div className={styles.techList}>
+              {/* techList안에 각 item을 보여준다.  */}
+              {techItems.map((item) => {
+                return (
+                  <div key={item.name} className="techItem">
+                    <img
+                      key={item.name}
+                      src={`https://cdn.simpleicons.org/${item.icon}/${item.color}`}
+                      alt={item.name}
+                    />
+                    <p>{item.name}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
     </div>
   );
 }
