798f7e2cc931af9eea0b1ee7c2c8adf559a1839e
Ga Min Cha <prettygamin@naver.com>
2026-03-01 13:46:08 +0900

    수정: Header-로고에 링크 추가

--- gamin/portfolio/src/components/Header/Header.tsx
+++ gamin/portfolio/src/components/Header/Header.tsx
@@ -5,11 +5,11 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logoWrapper}>
+      <Link to="/" className={styles.logoWrapper}>
         {/* 임시 이미지 크기 지정 */}
         <img src={logoImg} alt="로고 사진 (현대오토에버 고양이)" width={74} />
         <h1>가민's 포트폴리오</h1>
-      </div>
+      </Link>
       <nav className={styles.nav}>
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
