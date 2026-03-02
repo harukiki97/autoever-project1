f43fdc9e05a40920c0de74ab141d5d5963263c3c
YeEun Kim <kye1115z@naver.com>
2026-03-03 00:19:32 +0900

    변경: 경력 사항 스타일링

--- yeeun/portfolio/src/components/Career/CareerContent.tsx
+++ yeeun/portfolio/src/components/Career/CareerContent.tsx
@@ -6,25 +6,43 @@ const CareerContent = ({ title }: { title: CareerCategory }) => {
   const categoryData = careers.find((career) => career.category === title);
 
   if (!categoryData) return null;
+
   return (
-    <>
-      <div>
-        <div className={styles.lineTitle}>
-          <span>{categoryData?.category}</span>
-        </div>
-        {categoryData.items.map((item) => (
-          <div key={item.id}>
-            <div>
-              {title !== "활동" ? (
-                <img src="/assets/icons/Star.svg" />
-              ) : (
-                <img src="/assets/Octagram.svg" />
+    <div className={styles.container}>
+      <div className={styles.lineTitle}>
+        <span>{categoryData.category}</span>
+      </div>
+
+      {categoryData.items.map((item) => {
+        const isCurrent = item.period.includes("현재");
+
+        return (
+          <div key={item.id} className={styles.item}>
+            <div className={styles.date}>
+              <img
+                src={
+                  title !== "활동"
+                    ? isCurrent
+                      ? "/assets/icons/Star_color.svg"
+                      : "/assets/icons/Star.svg"
+                    : isCurrent
+                      ? "/assets/icons/Octagram_color.svg"
+                      : "/assets/icons/Octagram.svg"
+                }
+              />
+              <p>{item.period}</p>
+            </div>
+            <div className={styles.content}>
+              <p className={styles.title}>{item.title}</p>
+              <p className={styles.desc}>{item.desc}</p>
+              {item.desc_detail && (
+                <p className={styles.desc_detail}>{item.desc_detail}</p>
               )}
             </div>
           </div>
-        ))}
-      </div>
-    </>
+        );
+      })}
+    </div>
   );
 };
 
