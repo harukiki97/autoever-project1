120a10a57d08ffbac6559c90f72497e1a9ac2554
YeEun Kim <kye1115z@naver.com>
2026-03-03 08:00:58 +0900

    추가: floatingContact, Footer를 Home에 추가

--- yeeun/portfolio/src/pages/Home/Home.tsx
+++ yeeun/portfolio/src/pages/Home/Home.tsx
@@ -1,3 +1,5 @@
+import FloatingContact from "../../components/Contact/FloatingContact";
+import Footer from "../../components/Footer/Footer";
 import AboutMe from "../AbooutMe/AboutMe";
 import Career from "../Career/Career";
 import Posts from "../Posts/Posts";
@@ -23,10 +25,8 @@ const Home = () => {
       <section id="blog" className={styles.section}>
         <Posts />
       </section>
-      <section id="contacts" className={styles.section}>
-        <h2>Contacts</h2>
-        <p>연락처 정보를 여기에 추가하세요.</p>
-      </section>
+      <Footer />
+      <FloatingContact />
     </div>
   );
 };
