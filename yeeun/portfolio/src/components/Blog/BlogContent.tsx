7100efa57338c3c94e6a3fa297f5897144c44833
kye1115z <kye1115z@naver.com>
2026-03-03 11:17:53 +0900

    추가: 블로그 요소 컴포넌트 추가

---
+++ yeeun/portfolio/src/components/Blog/BlogContent.tsx
@@ -0,0 +1,24 @@
+import styles from "./BlogContent.module.css";
+import type { Post } from "../../types";
+import { formatRelativeDate } from "../../utils/getDate";
+import { Link } from "react-router-dom";
+
+interface Props {
+  post: Post;
+  type: "ALL" | "BLOG" | "TIL";
+}
+
+const BlogContent = ({ post, type }: Props) => {
+  return (
+    <Link to={`/blog/${post.id}`} className={styles.blogContent}>
+      <h2 className={styles.title}>{post.title}</h2>
+      <p className={styles.summary}>{post.summary}</p>
+      <div className={styles.bottomData}>
+        <span>{formatRelativeDate(post.created_at)}</span>
+        <span>{type}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default BlogContent;
