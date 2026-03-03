d2146ece37e6cb673c5f72e3c8f7d33fdec55bca
kye1115z <kye1115z@naver.com>
2026-03-03 10:20:52 +0900

    추가: 전체/상세 프로젝트 페이지, 블로그, 방명록 라우팅

--- yeeun/portfolio/src/App.tsx
+++ yeeun/portfolio/src/App.tsx
@@ -3,6 +3,10 @@ import styles from "./App.module.css";
 
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import AllProjects from "./pages/AllProjects/AllProjects";
+import Blog from "./pages/Blog/Blog";
+import GuestBook from "./pages/GuestBook/GuestBook";
+import ProjectDetail from "./pages/ProjectDetail/ProjectDetail";
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<AllProjects />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/guestbook" element={<GuestBook />} />
       </Routes>
     </div>
   );
